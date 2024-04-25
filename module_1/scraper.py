916992d9f3cd4f3fa19a502d862c93472d269972
AndreGrig0rian <andre.grigorian.149@my.csun.edu>
2024-04-25 01:08:06 -0700

    removed see all element, goes straight to fighter history

--- module_1/scraper.py
+++ module_1/scraper.py
@@ -1,39 +1,45 @@
+# 4/20/2024
+# This scraper crawls espn fight stats website to count up how many times a specified fighter has been taken down in the UFC
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+import re
 
 options = webdriver.FirefoxOptions()
 options.add_argument('-headless')
 driver = webdriver.Firefox(options=options)
-# driver = webdriver.Firefox()
+# driver = webdriver.Firefox() #uncomment to debug with auutomated browser open
 
-
-# This scraper scrapes espn fighter stats website to count up how many times a specified fighter has been taken down in the UFC
 
 def findFighter(name: str):
     fighterName = name.replace(" ", "%20").lower()
     url = f'https://www.espn.com/search/_/q/{fighterName}'
     driver.get(url)
-    time.sleep(3)  # wait for browser to load content
-
-    element = driver.find_element(
-        By.XPATH, '/html/body/div[1]/div/div/div/div/main/div[2]/div/div/div[3]/section[1]/div/ul/div')
+    # wait for browser to load content
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, '/html/body/div[1]/div/div/div/div/main/div[2]/div/div/div[3]/section[1]/div/ul/div'))
+    )
     fighterUrl = element.find_element(By.TAG_NAME, "a").get_attribute("href")
     return fighterUrl
 
 
 def numberOfTimesTakendown(name: str):
     # expand list of fighters fought
-    fighterUrl = findFighter(name)
+    fighterUrl = findFighter(name).replace('fighter', 'fighter/history')
     driver.get(fighterUrl)
-    time.sleep(2)
-    seeAllButton = driver.find_element(
-        By.XPATH, '/html/body/div[1]/div/div/div/div/main/div[2]/div[5]/div/div[2]/div[1]/section/header/div[2]/a/div')
-    seeAllButton.click()
 
-    table = driver.find_element(
-        By.XPATH, '/html/body/div[1]/div/div/div/div/main/div[2]/div[5]/div/div[1]/section/div/div/div/div/div[2]/table')
+    # wait for browser to load content
+    table = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, '/html/body/div[1]/div/div/div/div/main/div[2]/div[5]/div/div[1]/section/div/div/div/div/div[2]/table'))
+    )
+
     table = table.find_element(By.TAG_NAME, 'tbody')
     rows = table.find_elements(By.TAG_NAME, 'tr')
 
@@ -43,12 +49,10 @@ def numberOfTimesTakendown(name: str):
     # crawl through each opponents stats, tally how many times they've taken down specified fighter
     print('counting...')
     for rowIndex in range(len(rows)):
-        time.sleep(1)
         opponent = driver.find_element(
             By.XPATH, f'/html/body/div[1]/div/div/div/div/main/div[2]/div[5]/div/div[1]/section/div/div/div/div/div[2]/table/tbody/tr[{rowIndex+1}]/td[2]/a')
         opponentUrl = opponent.get_attribute("href")
         # only consider oppenents fought in the UFC
-        # if "ufc" in driver.find_element(By.XPATH, f'/html/body/div[1]/div/div/div/div/main/div[2]/div[5]/div/div[1]/section/div/div/div/div/div[2]/table/tbody/tr[{rowIndex+1}]/td[7]/a').text.lower():
         opponentUrls.append(opponentUrl)
 
     for opponentUrl in opponentUrls:
@@ -56,26 +60,31 @@ def numberOfTimesTakendown(name: str):
         # navigate to opponents stats
         statsUrl = opponentUrl.replace('fighter', 'fighter/stats')
         driver.get(statsUrl)
-        time.sleep(2)
         try:  # check if opponent has stats up
-            table = driver.find_element(
-                By.XPATH, '/html/body/div[1]/div/div/div/div/main/div[2]/div[5]/div/div[1]/div[1]/section/div/div[3]/div[2]/div/div[2]/table')
+            table = WebDriverWait(driver, 2).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '/html/body/div[1]/div/div/div/div/main/div[2]/div[5]/div/div[1]/div[1]/section/div/div[3]/div[2]/div/div[2]/table'))
+            )
         except Exception as e:
-            print(e)
+            print("opponent's ufc stats not availble.")
             continue
 
         table = table.find_element(By.TAG_NAME, 'tbody')
         opponentRows = table.find_elements(By.TAG_NAME, 'tr')
         for opponentRowIndex in range(len(opponentRows)):
-            OpponentsOpponent = driver.find_element(
+            opponentsOpponent = driver.find_element(
                 By.XPATH, f'/html/body/div[1]/div/div/div/div/main/div[2]/div[5]/div/div[1]/div[1]/section/div/div[3]/div[2]/div/div[2]/table/tbody/tr[{opponentRowIndex+1}]/td[2]/a')
             # only tally specified fighter's fight
-            if (OpponentsOpponent.get_attribute('href') == fighterUrl):
+            if (opponentsOpponent.get_attribute('href') == fighterUrl):
                 takedownAmmount = driver.find_element(
                     By.XPATH, f'/html/body/div[1]/div/div/div/div/main/div[2]/div[5]/div/div[1]/div[1]/section/div/div[3]/div[2]/div/div[2]/table/tbody/tr[{opponentRowIndex+1}]/td[13]')
-                timesTakenDown += int(takedownAmmount.text)
+                # clean any non integers in string
+                numOnly = re.findall(r'\d+', takedownAmmount.text)
+                numOnly = ''.join(numOnly)
+                if numOnly:
+                    timesTakenDown += int(numOnly)
     return timesTakenDown
 
 
-print(numberOfTimesTakendown("Arman Tsarukyan"))
+print(numberOfTimesTakendown("khabib nurmagomedov"))
 driver.quit()
